3e3dffb136fb45e2c26419e8d58d861471a306df
Viktoras Dulko <v.dulko@eisgroup.com>
2021-10-08 16:15:28 +0300

    my change

--- src/main/java/me/escoffier/lab/chapter1/Code1.java
+++ src/main/java/me/escoffier/lab/chapter1/Code1.java
@@ -8,17 +8,15 @@ import io.reactivex.*;
 public class Code1 {
 
     private static List<String> SUPER_HEROES = Arrays.asList(
-        "Superman",
-        "Batman",
-        "Aquaman",
-        "Asterix",
-        "Captain America"
+            "Superman",
+            "Batman",
+            "Aquaman",
+            "Asterix",
+            "Captain America"
     );
 
     public static void main(String... args) {
         Observable<String> stream = Observable.fromIterable(SUPER_HEROES);
-        stream.subscribe(
-            name -> System.out.println(name)
-        );
+        stream.subscribe(name -> System.out.println(name));
     }
 }
